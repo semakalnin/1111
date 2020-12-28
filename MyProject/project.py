279390475bebccdc6f525f042281d5eb92fa7b37
semakalnin <75793316+semakalnin@users.noreply.github.com>
2020-12-28 11:26:47 +0600

    Add files via upload

---
+++ MyProject/project.py
@@ -0,0 +1,259 @@
+word=input("Enter a word")
+if word=='arise':
+    print("arise arose arisen")
+elif word=='awake':
+    print("awake awoke awoken")
+elif word=='be':
+    print("be was/were been")
+elif word=='bear':
+    print("bear bore born(e)")
+elif word=='beat':
+    print==("beat  beat beaten")
+elif word=='become':
+    print("become became become")    
+elif word=='begin':
+    print("begin began begun")
+elif word=='bend':
+    print("bend bent bent")
+elif word=='bet':
+    print("bet bet bet")
+elif word=='bind':
+    print("bind bound bound")
+elif word=='bite':
+    print("bite bit bitten")
+elif word=='bleed':
+    print("bleed bled bled")
+elif word=='blow':
+    print("blow blew blown")
+elif word=='break':
+    print("break broke broken")
+elif word=='breed':
+    print("breed bred bred")
+elif word=='bring':
+    print("bring brought brought")
+elif word=='broadcast':
+    print("broadcast broadcast broadcast")
+elif word=='build':
+    print("build built built")
+elif word=='burn':
+    print("burn burnt/burned burnt/burned")
+elif word=='burst':
+    print("burst burst burst")
+elif word=='buy':
+    print("buy bought bought")
+elif word=='can':
+    print("can could ...(been able)")
+elif word=='catch':
+    print("catch cought cought")
+elif word=='choose':
+    print("choose chose chosen")
+elif word=='cling':
+    print("cling clung clung")
+elif word=='come':
+    print("come came come")
+elif word=='cost':
+    print("cost cost cost")
+elif word=='creep':
+    print("creep crept crept")
+elif word=='cut':
+    print("cut cut cut")
+elif word=='deal':
+    print("deal dealt dealt")
+elif word=='dig':
+    print("dig dug dug")
+elif word=='do':
+    print("do did done")
+elif word=='dream':
+    print("dream dreamt/dreamed dreamt/dreamed")
+elif word=='drink':
+    print("drink drank drunk")
+elif word=='drive':
+    print("drive drove driven")
+elif word=='eat':
+    print("eat ate eaten")
+elif word=='fall':
+    print("fall fell fallen")
+elif word=='feed':
+    print("feed fed fed")
+elif word=='fight':
+    print("fight fought fought")
+elif word=='find':
+    print("find found found")
+elif word=='fly':
+    print("fly flew flown")
+elif word=='forbid':
+    print("forbid forbade forbidden")
+elif word=='forget':
+    print("forget forgot forgotten")
+elif word=='forgive':
+    print("forgive forgave forgiven")
+elif word=='freeze':
+    print("freeze froze frozen")
+elif word=='get':
+    print("get got got")
+elif word=='give':
+    print("give gave given")
+elif word=='go':
+    print("go went gone")
+elif word=='grind':
+    print("grind ground ground")
+elif word=='grow':
+    print("grow grew grown")
+elif word=='hang':
+    print("hang hung hung")
+elif word=='have':
+    print("have had had")
+elif word=='hide':
+    print("hide hid hidden")
+elif word=='hit':
+    print("hit hit hit")
+elif word=='hold':
+    print("hold held held")
+elif word=='hurt':
+    print("hurt hurt hurt")
+elif word=='keep':
+    print("keep kept kept")
+elif word=='kneel':
+    print("kneel knelt knelt")
+elif word=='know':
+    print("know knew known")
+elif word=='lay':
+    print("lay laid laid")
+elif word=='lead':
+    print("lead led led")
+elif word=='lean':
+    print("lean leant/leaned leant/leaned")
+elif word=='learn':
+    print("learn learnt/learned learnt/learned")
+elif word=='leave':
+    print("leave left left")
+elif word=='lend':
+    print("lend lent lent")
+elif word=='lie':
+    print("lie lied lied")
+elif word=='light':
+    print("light lit/lighted lighted")
+elif word=='lose':
+    print("lose lost lost")
+elif word=='make':
+    print("make made made")
+elif word=='may':
+    print("may might might")
+elif word=='mean':
+    print("mean meant meant")
+elif word=='meet':
+    print("meet met met")
+elif word=='overtake':
+    print("overtake overtook overtaken")
+elif word=='pay':
+    print("pay paid paid")
+elif word=='put':
+    print("put put put")
+elif word=='read':
+    print("read read read")
+elif word=='ride':
+    print("ride rode ridden")
+elif word=='ring':
+    print("ring rang rung")
+elif word=='rise':
+    print("rise rose risen")
+elif word=='run':
+    print("run ran run")
+elif word=='saw':
+    print("saw sawed sawn/sawed")
+elif word=='say':
+    print("say said said")
+elif word=='see':
+    print("see saw seen")
+elif word=='sell':
+    print("sell sold sold")
+elif word=='send':
+    print("send sent sent")
+elif word=='set':
+    print("set set set")
+elif word=='sew':
+    print('sew sewed sewn/sewed')
+elif word=='shake':
+    print("shake skook shaken")
+elif word=='shall':
+    print("shall should ...")
+elif word=='shed':
+    print("shed shed shed")
+elif word=='shine':
+    print("shine shone shone")
+elif word=='shoot':
+    print("shoot shot shot")
+elif word=='show':
+    print("show showed shown")
+elif word=='shrink':
+    print("shrink shrank shrunk")
+elif word=='shut':
+    print("shut shut shut")
+elif word=='sing':
+    print("sing sang sung")
+elif word=='sink':
+    print("sink sank sunk")
+elif word=='sit':
+    print("sit sat sat")
+elif word=='sleep':
+    print("sleep slept slept")
+elif word=='slide':
+    print("slide slid slid")
+elif word=='smell':
+    print("smell smelt smelt")
+elif word=='sow':
+    print("sow sowed sown/sowed")
+elif word=='speak':
+    print("speak spoke spoken")
+elif word=='spell':
+    print("spell spelt/spelled spelt/spelled")
+elif word=='spend':
+    print("spend spent spent")
+elif word=='spill':
+    print("spll spilt/spilled spilt/spilled")
+elif word=='spread':
+    print("spread spread spread")
+elif word=='stand':
+    print("stand stood stood")
+elif word=='steal':
+    print("steal stole stolen")
+elif word=='stick':
+    print("stick stuck stuck")
+elif word=='sting':
+    print("sting stung stung")
+elif word=='strike':
+    print("strike struck struck")
+elif word=='swear':
+    print("swear swore sworn")
+elif word=='sweep':
+    print("sweep swept swept")
+elif word=='swell':
+    print("swell swelled swollen/swelled")
+elif word=='swim':
+    print("swim swam swum")
+elif word=='swing':
+    print("swing swung swung")
+elif word=='take':
+    print("take took taken")
+elif word=='teach':
+    print("teach taught taught")
+elif word=='throw':
+    print("throw threw thrown")
+elif word=='understand':
+    print("understand understood understood")
+elif word=='wake':
+    print("wake woke woken")
+elif word=='wear':
+    print("wear wore worn")
+elif word=='weep':
+    print("weep wept wept")
+elif word=='will':
+    print("will would ...")
+elif word=='win':
+    print("win won won")
+elif word=='wind':
+    print("wind wound wound")
+elif word=='write':
+    print("write wrote wtitten")
+else:
+    print("invalid spelling")
